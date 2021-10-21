fda8393be81861332a874ef0384a443977927e77
Sharvin Shah <sharvin.shah@salesforce.com>
2021-10-21 12:12:54 -0700

    add func to sample data from csv and save to json

--- taichi/data_pipeline.py
+++ taichi/data_pipeline.py
@@ -25,6 +25,27 @@ class DataPipeline(object):
         self.raw_data = None
 
 
+    def sample_from_csv(self, n_shot=None):
+        """
+        expects a file in csv format as follows:
+        utterance,language,label (no headers and no index)
+        
+        Example:
+        book a ticket from San Francisco to New York,en_US,Book a Flight
+        
+        returns a subsampled pandas dataframe with utterance, language and label columns
+        """        
+        df = pd.read_csv(self.data_path, names=['utterance', 'language', 'label'])
+        examples_per_class = dict(Counter((df.label)))
+        minimum_examples_per_class = min(examples_per_class.values())
+        if minimum_examples_per_class >= n_shot:
+            subsampled_df = df.groupby('label').sample(n=n_shot, random_state=0)
+            return subsampled_df
+        else:
+            error_message = "number of examples per class are not enough to sample based on n_shot={} value".format(n_shot)
+            raise Exception(error_message)        
+
+
     def sample_from_json(self, n_shot=None, split='train'):
         """
         expects a file in json format as follows:
@@ -53,13 +74,33 @@ class DataPipeline(object):
             raise Exception(error_message)
 
 
-    def save_subsampled_data_to_csv(self, save_dir, n_shot=None, split='train'):
+    def save_subsampled_data_to_csv(self, save_dir, n_shot=None, split='train', is_json=False):
         """
-        saves the subsampled data from json into csv with utterance,
+        saves the subsampled data from csv/json into csv with utterance,
         language and label columns with no headers and no index in the requisite
         save directory
         """
-        subsampled_df = self.sample_from_json(n_shot=n_shot, split=split)
+        if is_json:
+            subsampled_df = self.sample_from_json(n_shot=n_shot, split=split)
+        else:
+            subsampled_df = self.sample_from_csv(n_shot=n_shot)
+
         save_path = os.path.join(save_dir, self.name + '_' + str(n_shot) + 
                                 '_shot_' + split + '.csv')
         subsampled_df.to_csv(save_path, header=None, index=None)
+
+
+    def save_subsampled_data_to_json(self, save_dir, n_shot=None, split='train', is_json=False):
+        """
+        saves the subsampled data from csv/json into json with individual utterance,
+        language and label values as an independent array in the requisite
+        save directory
+        """
+        if is_json:
+            subsampled_df = self.sample_from_json(n_shot=n_shot, split=split)
+        else:
+            subsampled_df = self.sample_from_csv(n_shot=n_shot)
+
+        save_path = os.path.join(save_dir, self.name + '_' + str(n_shot) + 
+                                '_shot_' + split + '.json')
+        subsampled_df.to_json(save_path, orient='values')        
