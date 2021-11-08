d0573d32e01243b810cb127bc34b51d7b4d88a06
Sharvin Shah <sharvin.shah@salesforce.com>
2021-11-08 11:14:46 -0800

    separate config from model files

--- taichi/dnnc.py
+++ taichi/dnnc.py
@@ -38,72 +38,13 @@ from sklearn.metrics import (
 
 import copy
 
+from taichi.config import Config
 from taichi.error_analysis import ErrorAnalysis
-
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-
-class Config:
-    """
-    A simple wrapper around a dictionary type
-    that supports two different ways to retrieve the value
-    from the key for convenience & easier code search accessing the attribute.
-    A. config.key
-    B. config[key]
-    Support most of the builtin functions of a dictionary, such as
-    len(config)
-    for x in config:
-    print(config)
-    x in config
-    for k,v in config.items():
-    should be supported
-
-    adapted from https://github.com/MetaMind/eloq_dial/blob/main/config.py
-    """
-
-    def __init__(self, dictionary: dict = None):
-        if dictionary is None:
-            dictionary = dict()
-        self._keys = set(dictionary)
-        for key, value in dictionary.items():
-            self.__setattr__(key, value)
-
-    def __repr__(self):
-        d = self.__dict__.copy()
-        del d["_keys"]  # remove keys attribute from printing
-        return d.__repr__()
-
-    def copy(self):
-        """ Creates a deep copy of this instance """
-        return copy.deepcopy(self)
-
-    def __getitem__(self, item):
-        return self.__getattribute__(item)
-
-    def __contains__(self, item) -> bool:
-        return item in self._keys
-
-    def __iter__(self) -> Iterable[str]:
-        return self._keys.__iter__()
-
-    def items(self) -> Iterable[Tuple]:
-        return ((key, self[key]) for key in self)
-
-    def __len__(self) -> int:
-        return len(self._keys)
-
-    def __eq__(self, other):
-        if len(self) != len(other):
-            return False
-        for key, value in other.items():
-            if key not in self._keys or value != self[key]:
-                return False
-        return True
-
 
 
 class DNNC(object):
