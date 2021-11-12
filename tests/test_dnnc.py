733fd57852b96d65ecd986b281ded2e4ce085de3
Sharvin Shah <sharvin.shah@salesforce.com>
2021-11-12 12:07:07 -0800

    add check to verify results

--- tests/test_dnnc.py
+++ tests/test_dnnc.py
@@ -45,9 +45,11 @@ class ExperimentRunTest(unittest.TestCase):
         logger.info('checking if results have been saved...')
         self.assertTrue(os.path.exists(d.config.error_analysis_dir), 'error analysis folder was not created!') # change path to save_results_fp in test_uslp_config
         files = [f for f in os.walk(d.config.error_analysis_dir)]
-        self.assertEqual(len(files[0][-1]), 5, 'error analysis files are missing!') # count number of files in saved_model_dir        
+        self.assertEqual(len(files[0][-1]), 5, 'error analysis files are missing!') # count files in saved_model_dir
+        self.assertTrue(os.path.exists(d.config.save_result_fp), 'results file missing') # check if result file created 
         shutil.rmtree(d.config.saved_model_path)
         shutil.rmtree(d.config.error_analysis_dir)
+        os.remove(d.config,save_result_fp)
         logger.info('testing completed!')
 
 if __name__ == '__main__':
