ac8910ba0b6ed3f6d0b577419f7bd11f97834ef0
Sharvin Shah <sharvin.shah@salesforce.com>
2021-11-12 12:11:04 -0800

    remove unnecessary logs and comments

--- tests/test_uslp.py
+++ tests/test_uslp.py
@@ -35,18 +35,17 @@ class ExperimentRunTest(unittest.TestCase):
         u.train()
         logger.info('model trained')
         logger.info('checking if trained model has been saved...')
-        self.assertTrue(os.path.exists(u.config.saved_model_path), 'saved model folder was not created!') # change path to saved_model_dir in test_uslp_config
+        self.assertTrue(os.path.exists(u.config.saved_model_path), 'saved model folder was not created!')
         files = [f for f in os.walk(u.config.saved_model_path)]
-        logger.info(files[0])
-        self.assertEqual(len(files[0][-1]), 2, 'saved model file is missing!') # count number of files in saved_model_dir
+        self.assertEqual(len(files[0][-1]), 2, 'saved model file is missing!') # count files in saved_model_dir
         logger.info('model was saved successfully!')
         logger.info('begin the model evaluation...')
         u.eval()
         logger.info('checking if results have been saved...')
-        self.assertTrue(os.path.exists(u.config.error_analysis_dir), 'error analysis folder was not created!') # change path to save_results_fp in test_uslp_config
+        self.assertTrue(os.path.exists(u.config.error_analysis_dir), 'error analysis folder was not created!')
         files = [f for f in os.walk(u.config.error_analysis_dir)]
         self.assertEqual(len(files[0][-1]), 5, 'error analysis files are missing!') # count files in saved_model_dir
-        self.assertTrue(os.path.exists(u.config.save_result_fp), 'results were not generated') # check if result file created
+        self.assertTrue(os.path.exists(u.config.save_result_fp), 'results file missing') # check if result file created
         shutil.rmtree(u.config.saved_model_path)
         shutil.rmtree(u.config.error_analysis_dir)
         os.remove(u.config.save_result_fp)
